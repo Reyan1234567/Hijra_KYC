823c9c82333a22f5e57a144158bf62c60b868426
Reyan1234567 <reyann.14b@gmail.com>
2025-07-01 16:04:15 +0300

    maven initial starter code

---
+++ src/main/java/com/example/hijra_kyc/HijraKycApplication.java
@@ -0,0 +1,13 @@
+package com.example.hijra_kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HijraKycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HijraKycApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hijra_kyc/HijraKycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hijra_kyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HijraKycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
