7cced10d6361bce231a8864a2734f74bf66d2043
Reyan1234567 <reyann.14b@gmail.com>
2025-07-07 08:32:30 +0300

    correction on globalException handler

--- src/main/java/com/example/hijra_kyc/exception/GlobalException.java
+++ src/main/java/com/example/hijra_kyc/exception/GlobalException.java
@@ -2,6 +2,7 @@ package com.example.hijra_kyc.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,9 +15,14 @@ public class GlobalException {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         var errors=new HashMap<String, String>();
-        exception.getBindingResult().getAllErrors().forEach((error)->{errors.put(error.getDefaultMessage(),error.getDefaultMessage());});
+        exception.getBindingResult().getAllErrors().forEach((error)->{errors.put(((FieldError)error).getField(),error.getDefaultMessage());});
         return ResponseEntity.badRequest().body(errors);
     }
 }
 
 
+
+//exception.getBindingResult():- gives a BindingResult object, which has a method getAllErrors() that returns
+//List<ObjectError>
+//ObjectError is the parent of FieldError
+//we cast it to FieldErrors and iteratively put it to the map we declared, extracting the name and the value of the validation error
