11deb74cc161b1569f31f68765e84dcceee74c38
Reyan1234567 <reyann.14b@gmail.com>
2025-08-03 18:18:17 +0300

    image reduction logic refined

--- src/main/java/com/example/hijra_kyc/util/FileUpload.java
+++ src/main/java/com/example/hijra_kyc/util/FileUpload.java
@@ -5,10 +5,10 @@ import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Base64;
 
 @Slf4j
@@ -20,12 +20,6 @@ public class FileUpload {
                 throw new RuntimeException("file type not supported");
             }
 
-            String encodedBit = base64.split(",")[1];
-            byte[] fileByte = Base64.getDecoder().decode(encodedBit);
-
-            System.out.println(type);
-            //check, if really an image
-
             //creating the path's parent path
             File uploadDir = new File(parent);
             //check existence and create if path doesn't exist
@@ -36,12 +30,34 @@ public class FileUpload {
             //the full path which includes the file to be created
             File fileToBeUploaded = new File(child);
 
+            String encodedBit = base64.split(",")[1];
+            byte[] fileByte = Base64.getDecoder().decode(encodedBit);
+
+            InputStream is=new ByteArrayInputStream(fileByte);
+            BufferedImage image = ImageIO.read(is);
+
+            int newWidth = 800;
+            int newHeight=(newWidth*image.getHeight())/image.getWidth();
+
+            BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+
+            Graphics2D g2d=resizedImage.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2d.drawImage(image, 0, 0, newWidth, newHeight, null);
+            g2d.dispose();
+
+
+            File output = new File(child);
+            ImageIO.write(resizedImage, type, output);
 
 //          creates the file in a lower storage and detail
-            Thumbnails.of(new ByteArrayInputStream(fileByte))
-                    .size(1000, 1000)
-                    .outputQuality(1)
-                    .toFile(fileToBeUploaded);
+//            Thumbnails.of(new ByteArrayInputStream(fileByte))
+//                    .size(500, 500)
+//                    .outputQuality(0.95)
+//                    .toFile(fileToBeUploaded);
 //          could use Files.write(Path, bytes), were bytes is the decoded image,
 //          and the the Path is of type path representing the path(Path.get("..."))}
 //          or Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING); if file is of type Multi-part
